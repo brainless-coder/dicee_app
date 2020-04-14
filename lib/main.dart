9aabf97899e8a19fb79a770d7305f7bd45e09dce
Prime <singhamish27@gmail.com>
2020-04-14 12:23:56 +0530

    Updated the dice

--- lib/main.dart
+++ lib/main.dart
@@ -18,19 +18,25 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var leftDiceNumber = 5;
+
     return Center(
       child: Row(
         children: <Widget>[
           Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Image.asset('images/dice1.png'),
+            child: FlatButton(
+              onPressed: () {
+                print('Left Button Pressed');
+              },
+              child: Image.asset('images/dice$leftDiceNumber.png'),
             ),
           ),
           Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Image.asset('images/dice2.png'),
+            child: FlatButton(
+              onPressed: () {
+                print("Right Button Pressed");
+              },
+              child: Image.asset('images/dice1.png'),
             ),
           ),
         ],
